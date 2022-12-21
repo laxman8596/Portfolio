1c9991908d38ab4897c1dcee3799496971e6170e
laxman8596 <90377590+laxman8596@users.noreply.github.com>
2022-12-21 13:20:20 +0530

    Add files via upload

--- src/Componets/ApiData.js
+++ src/Componets/ApiData.js
@@ -6,13 +6,13 @@ const Data = [
 		id: 1,
 		titel: 'My Resturant',
 		images: '../images/food.jpg',
-		url: 'https://food---app.herokuapp.com/'
+		url: 'https://resturant-website1.netlify.app/'
 	},
 	{
 		id: 2,
 		titel: 'Gallarey App',
 		images: './images/gallery.jpg',
-		url: 'https://gallarey-app.herokuapp.com/'
+		url: 'https://gallery-website11.netlify.app/'
 	},
 
 
@@ -21,7 +21,7 @@ const Data = [
 		id: 3,
 		titel: 'Weather App',
 		images: './images/weather.jpg',
-		url: 'https://weather--app9.herokuapp.com/'
+		url: 'https://weather-website1.netlify.app/'
 	},
 ]
 
